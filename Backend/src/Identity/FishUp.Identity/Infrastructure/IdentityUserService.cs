8bfdf78488c73c4988d0ce3a9877360ddaa2382d
Maciej Donczyk <donczyk.maciej@gmail.com>
2021-03-20 21:54:55 +0100

    added logging into file and console

--- Backend/src/Identity/FishUp.Identity/Infrastructure/IdentityUserService.cs
+++ Backend/src/Identity/FishUp.Identity/Infrastructure/IdentityUserService.cs
@@ -5,16 +5,19 @@ using System.Threading.Tasks;
 using FishUp.Identity.Infrastructure.EF;
 using FishUp.Identity.Messages.Commands;
 using FishUp.Identity.Responses;
+using Microsoft.Extensions.Logging;
 
 namespace FishUp.Identity.Infrastructure
 {
     public class IdentityUserService : IIdentityUserService
     {
         private readonly IdentityDbContext _identityDbContext;
+        private readonly ILogger<IdentityUserService> _logger;
 
-        public IdentityUserService(IdentityDbContext identityDbContext)
+        public IdentityUserService(IdentityDbContext identityDbContext, ILogger<IdentityUserService> logger)
         {
             _identityDbContext = identityDbContext;
+            _logger = logger;
         }
 
         public async Task<CreateUserResponse> CreateUserAsync(CreateUserRequest request, CancellationToken cancellationToken)
@@ -24,6 +27,7 @@ namespace FishUp.Identity.Infrastructure
                 var user = new User(request.Username, request.Email, request.Password, request.Password);
                 await _identityDbContext.Users.AddAsync(user);
                 await _identityDbContext.SaveChangesAsync();
+                
                 return new CreateUserResponse() 
                 {
                     Created = true
@@ -31,8 +35,9 @@ namespace FishUp.Identity.Infrastructure
             }
             catch(Exception ex)
             {
-                
+                _logger.LogError($"Registration error: {ex.Message}");
             }
+
             return new CreateUserResponse()
             {
                 Created = false,
--- Backend/src/Identity/FishUp.Identity/Startup.cs
+++ Backend/src/Identity/FishUp.Identity/Startup.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using FishUp.Identity.Infrastructure;
 using FishUp.Identity.Infrastructure.EF;
 using Microsoft.AspNetCore.Builder;
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace FishUp.Identity
 {
@@ -32,8 +34,11 @@ namespace FishUp.Identity
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            var path = Directory.GetCurrentDirectory();  
+            loggerFactory.AddFile($"{path}\\Logs\\Log.txt");
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
